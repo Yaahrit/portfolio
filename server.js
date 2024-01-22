0c760070c2d13819f699fe8898032157a95b864f
Yash raj <124022190+Yaahrit@users.noreply.github.com>
2024-01-22 12:18:28 +0530

    error resolve

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ let connection = mysql.createConnection({
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  port: process.env.PORT
+  port: process.env.DB_PORT // Use a different port for MySQL
 });
 
 // Define a route to serve your index file
